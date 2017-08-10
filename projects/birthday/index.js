2c80045f82102ca2311b6da0f030e8c619d79509
gamernes9 <gamernes9@gmail.com>
2017-08-10 01:21:09 -0500

    added github

---
+++ projects/birthday/index.js
@@ -0,0 +1,60 @@
+$('#button1').click(function() {
+  $(this).parent().parent().remove();
+});
+$('#button2').click(function() {
+  $(this).parent().parent().remove();
+});
+$('#button3').click(function() {
+  $(this).parent().parent().remove();
+});
+$('#button4').click(function() {
+  $(this).parent().parent().remove();
+});
+$('#button5').click(function() {
+  $(this).parent().parent().remove();
+});
+$('#button6').click(function() {
+  $(this).parent().parent().remove();
+});
+$('#button7').click(function() {
+  $(this).parent().parent().remove();
+});
+$('#button8').click(function() {
+  $(this).parent().parent().remove();
+});
+$('#button9').click(function() {
+  $(this).parent().parent().remove();
+});
+$('#button10').click(function() {
+  $(this).parent().parent().remove();
+});
+$('#button11').click(function() {
+  $(this).parent().parent().remove();
+});
+$('#button12').click(function() {
+  $(this).parent().parent().remove();
+});
+$('#button13').click(function() {
+  $(this).parent().parent().remove();
+});
+$('#button14').click(function() {
+  $(this).parent().parent().remove();
+});
+$('#button15').click(function() {
+  $(this).parent().parent().remove();
+});
+$('#button16').click(function() {
+  $(this).parent().parent().remove();
+});
+$('#button17').click(function() {
+  $(this).parent().parent().remove();
+});
+$('#button18').click(function() {
+  $(this).parent().parent().remove();
+});
+$('#button19').click(function() {
+  $(this).parent().parent().remove();
+});
+$('#button20').click(function() {
+  $(this).parent().parent().remove();
+});
